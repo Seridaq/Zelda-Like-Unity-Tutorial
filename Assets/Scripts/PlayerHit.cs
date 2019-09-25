53cc6ac8acbddd54eb4297f1972c6a8e89b05459
Thomas <tebruhnke@gmail.com>
2019-09-25 08:05:00 -0500

    Added breakable pots animations and prefabs
    + Animated breakable pot
    + Player Prefab
    + Pot Prefab

---
+++ Assets/Scripts/PlayerHit.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHit : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("breakable"))
+        {
+            other.GetComponent<Pot>().OnBreak();
+        }
+    }
+}
